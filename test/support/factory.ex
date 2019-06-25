d71a9452a19d0ba1a68dcf55d5663d8a93d58c09
Steven Warren <steven@stevencwarren.com>
2019-06-24 22:52:10 -0500

    Fixes user factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,7 +2,7 @@ defmodule Stevencwarren.Factory do
   use ExMachina.Ecto, repo: Stevencwarren.Repo
 
   def user_factory do
-    %Stevencwarren.User{
+    %Stevencwarren.UserManager.User{
       email: sequence(:email, &"email-#{&1}@example.com"),
       password: "0U812"
     }
