df1a68d063d85030140362ca63d7c228414ff6a2
Steven Warren <steven@stevencwarren.com>
2019-06-24 23:24:15 -0500

    Fixes test, removes boiler plate

--- test/stevencwarren_web/controllers/page_controller_test.exs
+++ test/stevencwarren_web/controllers/page_controller_test.exs
@@ -3,6 +3,7 @@ defmodule StevencwarrenWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+
+    assert html_response(conn, 200) =~ "Hello, welcome to my small section of the internet."
   end
 end
