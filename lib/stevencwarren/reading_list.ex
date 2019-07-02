873cf92a6ae337d2e1bce775efa663da2d2a46d7
Steven Warren <steven@stevencwarren.com>
2019-07-02 17:03:16 -0500

    Adds list_categories to the ReadingList context

--- lib/stevencwarren/reading_list.ex
+++ lib/stevencwarren/reading_list.ex
@@ -1,10 +1,15 @@
 defmodule Stevencwarren.ReadingList do
   alias Stevencwarren.Repo
   alias Stevencwarren.ReadingList.Category
+  import Ecto.Query
 
   def create_category(name) do
     changeset = Category.changeset(%Category{}, %{name: name})
 
     Repo.insert(changeset)
+  end
+
+  def list_categories do
+    order_by(Category, asc: :name) |> Repo.all()
   end
 end
--- test/stevencwarren/reading_list_test.exs
+++ test/stevencwarren/reading_list_test.exs
@@ -2,6 +2,8 @@ defmodule Stevencwarren.ReadingListTest do
   use Stevencwarren.DataCase
 
   alias Stevencwarren.Repo
+  import Stevencwarren.Factory
+
   alias Stevencwarren.ReadingList
   alias Stevencwarren.ReadingList.Article
   alias Stevencwarren.ReadingList.Category
@@ -9,12 +11,25 @@ defmodule Stevencwarren.ReadingListTest do
   describe "articles" do
   end
 
-  describe "create_category" do
+  describe "create_category/1" do
     test "it creates a category from the supplied params" do
       {:ok, category} = ReadingList.create_category("test category")
 
       assert category.name == "test category"
       assert Repo.aggregate(Category, :count, :id) == 1
+    end
+  end
+
+  describe "list_categories/0" do
+    test "it returns all of the categories in alphabetical order" do
+      category_Z = insert(:category, %{name: "Z category" })
+      category_B = insert(:category, %{name: "B category" })
+      category_A = insert(:category, %{name: "A category" })
+      category_Y = insert(:category, %{name: "Y category" })
+
+      categories = ReadingList.list_categories
+
+      assert categories == [category_A, category_B, category_Y, category_Z]
     end
   end
 end
