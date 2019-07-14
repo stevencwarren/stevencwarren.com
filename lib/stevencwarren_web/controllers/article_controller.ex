301de34107c4ff154165b374a17cba6ac235b9ac
Steven Warren <steven@stevencwarren.com>
2019-07-14 12:20:55 -0500

    Adds recently added articles to the reading list index

--- lib/stevencwarren_web/controllers/article_controller.ex
+++ lib/stevencwarren_web/controllers/article_controller.ex
@@ -5,7 +5,8 @@ defmodule StevencwarrenWeb.ArticleController  do
 
   def index(conn, _) do
     categories = ReadingList.list_categories
-    render(conn, "index.html", categories: categories)
+    recent_articles = ReadingList.recent_articles
+    render(conn, "index.html", categories: categories, recent_articles: recent_articles)
   end
 
   def new(conn, _) do
@@ -15,7 +16,7 @@ defmodule StevencwarrenWeb.ArticleController  do
 
   def create(conn, %{"article" => article_params}) do
     case ReadingList.create_article(article_params) do
-      {:ok, article } ->
+      {:ok, _article } ->
         conn
         |> redirect(to: Routes.article_path(conn, :index))
 
