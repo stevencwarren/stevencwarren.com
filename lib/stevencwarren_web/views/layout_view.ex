b6d6dd2b404918630452ba093c1e125590da3329
Steven Warren <steven@stevencwarren.com>
2019-07-16 20:36:43 -0500

    Fixes scss loading and adds mark as read link

--- lib/stevencwarren_web/views/layout_view.ex
+++ lib/stevencwarren_web/views/layout_view.ex
@@ -1,3 +1,8 @@
 defmodule StevencwarrenWeb.LayoutView do
   use StevencwarrenWeb, :view
+  alias Stevencwarren.UserManager.Guardian
+
+  def logged_in?(conn)  do
+    Guardian.Plug.authenticated?(conn)
+  end
 end
