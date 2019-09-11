e7b60f487d9d244314ef6bf01559b0e11fa12607
Steven Warren <steven@stevencwarren.com>
2019-09-11 15:03:25 -0500

    Adds build commands

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :stevencwarren, StevencwarrenWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
@@ -55,11 +55,10 @@ config :logger, level: :info
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :stevencwarren, StevencwarrenWeb.Endpoint, server: true
+config :stevencwarren, StevencwarrenWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "/etc/secrets/prod.secrets.exs"
