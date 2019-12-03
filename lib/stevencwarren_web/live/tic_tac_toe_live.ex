33cb8275148f7c3712c6f7e1c1945a021f7cbf0b
Steven Warren <steven@stevencwarren.com>
2019-12-03 00:07:25 -0600

    Updated live view to add an X for every click in a square

--- lib/stevencwarren_web/live/tic_tac_toe_live.ex
+++ lib/stevencwarren_web/live/tic_tac_toe_live.ex
@@ -2,12 +2,26 @@ defmodule StevencwarrenWeb.TicTacToeLive do
   use Phoenix.LiveView
 
   alias StevencwarrenWeb.TicTacToeView
+  alias TicTacToe.{GameSupervisor, Game}
 
   def render(assigns) do
+    game = Game.via_tuple("foo")
+
     TicTacToeView.render("index.html", assigns)
   end
 
   def mount(_session, socket) do
-    {:ok, socket}
+    GameSupervisor.start_game("foo")
+    game = Game.via_tuple("foo")
+
+    {:ok, assign(socket, game: Game.get_state(game))}
+  end
+
+  def handle_event("mark", %{"index" => index}, socket) do
+    game = Game.via_tuple("foo")
+
+    Game.add_mark(game, String.to_integer(index), "X")
+
+    {:noreply, assign(socket, game: Game.get_state(game))}
   end
 end
