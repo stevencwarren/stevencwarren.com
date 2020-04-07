619eba82080013e1461728d4f9c7cc6b3ccf49bb
Steven Warren <steven@stevencwarren.com>
2020-04-07 13:06:53 -0500

    Adds resume and updates work history

--- lib/stevencwarren_web/endpoint.ex
+++ lib/stevencwarren_web/endpoint.ex
@@ -12,8 +12,8 @@ defmodule StevencwarrenWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :stevencwarren,
-    gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    gzip: true,
+    only: ~w(css fonts files images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
